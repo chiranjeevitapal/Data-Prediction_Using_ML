e61e37f345a7f871fbb49070268e404fb60becee
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2018-07-21 16:09:58 +0530

    first commit

---
+++ tester.py
@@ -0,0 +1,14 @@
+import pandas as pd  # data processing, CSV file I/O
+from sklearn.externals import joblib
+
+forest_model = joblib.load('persistence.pkl')
+test = pd.read_csv('test1.csv')  # Read the test data
+predict_cols = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+# Treat the test data in the same way as training data. In this case, pull same columns.
+test_X = test[predict_cols]
+predict_vals = forest_model.predict(test_X)  # make predictions using the model and test values
+print("Making predictions for the following houses:")
+
+print(test_X)
+print("The predictions are")
+print(predict_vals)  # print predicted values
---
+++ trainer.py
@@ -0,0 +1,17 @@
+import pandas as pd  # data processing, CSV file I/O
+from sklearn.ensemble import RandomForestRegressor  # library for Random Forest model
+from sklearn.externals import joblib
+
+train_file_path = 'train.csv'  # store file path
+train = pd.read_csv(train_file_path)  # read the training data
+print(train.describe())  # print data
+print(train.columns)  # print name of columns
+
+y = train.SalePrice  # set single column salePrice as target column
+predict_cols = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+train_X = train[predict_cols]  # set list of columns predic_cols as predictors
+
+forest_model = RandomForestRegressor()  # create random forest model
+forest_model.fit(train_X, y)  # train the model using predictors and target values
+
+joblib.dump(forest_model, 'persistence.pkl')
