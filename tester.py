592a788c2218af7e3bdcb1d69b0adc6dd017e3dd
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2018-07-22 09:57:50 +0530

    fixed issues

--- tester.py
+++ tester.py
@@ -2,11 +2,21 @@ import pandas as pd  # data processing, CSV file I/O
 from sklearn.externals import joblib
 
 forest_model = joblib.load('data/persistence_employee.pkl')
-test = pd.read_csv('data/employee_all_designatins_test_data.csv')  # Read the test data
+
+# Read Input file
+df_actual = pd.read_csv('data/employee_test_data.csv')
+# Read sample excel will all designations
+df_dummy = pd.read_csv('data/employee_all_designatins_test_data.csv')
+
+# Append sample to input file
+df_actual = df_actual.append(df_dummy)
+print('data after append')
+print(df_actual)
+
 predict_cols = ['Complexity', 'Time_Difference', 'Designation', 'Positive_Feedback', 'Negative_Feedback']
 
 # Treat the test data in the same way as training data. In this case, pull same columns.
-test_X = test[predict_cols]
+test_X = df_actual[predict_cols]
 
 pd.get_dummies(test_X['Designation'], prefix=['Designation'])
 test_X = pd.concat([test_X, pd.get_dummies(test_X['Designation'], prefix='Designation')], axis=1)
@@ -15,13 +25,25 @@ test_X.drop(['Designation'], axis=1, inplace=True)
 # Get prediction values
 predict_vals = forest_model.predict(test_X)  # make predictions using the model and test values
 
+print('Predict values')
+print(predict_vals)
+
 # Add a new column in the df for prediction values
 test_X['PREDS'] = predict_vals
 
 # Create a new df with the merged columns
-df_out = pd.merge(test, test_X[['PREDS']], how='left', left_index=True, right_index=True)
+#df_merge = pd.merge(df_actual, test_X[['PREDS']], how='inner', left_index=True, right_index=True)
+
+df_merge = pd.merge(df_actual.assign(key=df_actual.groupby(level=0).cumcount()).reset_index(),
+                    test_X.assign(key=test_X.groupby(level=0).cumcount()).reset_index(),
+                    how='inner', on=['index', 'key']). \
+    drop('key', 1).set_index('index')
+
+print('Data after merge')
+print(df_merge)
 
 print('Data after predictions')
+df_out = df_merge[df_merge.Assignee.str.contains("dummy") == False]
 print(df_out)
 
 # Calculate average performance of an employee
--- trainer.py
+++ trainer.py
@@ -1,23 +1,33 @@
 import pandas as pd  # data processing, CSV file I/O
 from sklearn.ensemble import RandomForestRegressor  # library for Random Forest model
 from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
 
 predict_cols = ['Complexity', 'Time_Difference', 'Designation', 'Positive_Feedback', 'Negative_Feedback']
+# read csv file
+train_file_path = 'data/employee_train_data.csv'
+df = pd.read_csv(train_file_path)  # read the training data
 
-train_file_path = 'data/employee_train_data.csv'  # store file path
-train = pd.read_csv(train_file_path)  # read the training data
+# set single column salePrice as target column
+predict_df = df.Performance_Rating
 
-y = train.Performance_Rating  # set single column salePrice as target column
+# set list of columns predict_cols as predictors
+train_df = df[predict_cols]
 
-train_X = train[predict_cols]  # set list of columns predic_cols as predictors
-# train_X = pd.get_dummies(train_X)
-pd.get_dummies(train_X['Designation'], prefix=['Designation'])
-train_X = pd.concat([train_X, pd.get_dummies(train_X['Designation'], prefix='Designation')], axis=1)
-train_X.drop(['Designation'], axis=1, inplace=True)
+pd.get_dummies(train_df['Designation'], prefix=['Designation'])
+train_df = pd.concat([train_df, pd.get_dummies(train_df['Designation'], prefix='Designation')], axis=1)
+train_df.drop(['Designation'], axis=1, inplace=True)
 
-print(train_X.columns)
+print(train_df.columns)
+
+# Create the X and y arrays
+X = train_df.values
+y = predict_df.values
+
+# Split the data set in a training set (70%) and a test set (30%)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 forest_model = RandomForestRegressor()  # create random forest model
-forest_model.fit(train_X, y)  # train the model using predictors and target values
+forest_model.fit(X_train, y_train)  # train the model using predictors and target values
 
 joblib.dump(forest_model, 'data/persistence_employee.pkl')
