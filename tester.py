06df48803ad8069297a61fcb9994d37e88bf158f
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2018-07-22 08:46:51 +0530

    employee rating predictions

--- tester.py
+++ tester.py
@@ -1,17 +1,16 @@
 import pandas as pd  # data processing, CSV file I/O
 from sklearn.externals import joblib
 
-forest_model = joblib.load('data/persistenceweather.pkl')
-test = pd.read_csv('data/rainfall_in_india_test_data.csv')  # Read the test data
-predict_cols = ['SUBDIVISION', 'YEAR', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV',
-                'DEC']
+forest_model = joblib.load('data/persistence_employee.pkl')
+test = pd.read_csv('data/employee_all_designatins_test_data.csv')  # Read the test data
+predict_cols = ['Complexity', 'Time_Difference', 'Designation', 'Positive_Feedback', 'Negative_Feedback']
 
 # Treat the test data in the same way as training data. In this case, pull same columns.
 test_X = test[predict_cols]
 
-pd.get_dummies(test_X['SUBDIVISION'], prefix=['SUBDIVISION'])
-test_X = pd.concat([test_X, pd.get_dummies(test_X['SUBDIVISION'], prefix='SUBDIVISION')], axis=1)
-test_X.drop(['SUBDIVISION'], axis=1, inplace=True)
+pd.get_dummies(test_X['Designation'], prefix=['Designation'])
+test_X = pd.concat([test_X, pd.get_dummies(test_X['Designation'], prefix='Designation')], axis=1)
+test_X.drop(['Designation'], axis=1, inplace=True)
 
 # Get prediction values
 predict_vals = forest_model.predict(test_X)  # make predictions using the model and test values
@@ -26,5 +25,5 @@ print('Data after predictions')
 print(df_out)
 
 # Calculate average performance of an employee
-merge_df_average_by_state = df_out.groupby('SUBDIVISION').mean()
+merge_df_average_by_state = df_out.groupby('Assignee').mean()
 print(merge_df_average_by_state)
--- trainer.py
+++ trainer.py
@@ -2,23 +2,22 @@ import pandas as pd  # data processing, CSV file I/O
 from sklearn.ensemble import RandomForestRegressor  # library for Random Forest model
 from sklearn.externals import joblib
 
-predict_cols = ['SUBDIVISION', 'YEAR', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV',
-                'DEC']
+predict_cols = ['Complexity', 'Time_Difference', 'Designation', 'Positive_Feedback', 'Negative_Feedback']
 
-train_file_path = 'data/rainfall_in_india_1901-2015_train_data.csv'  # store file path
+train_file_path = 'data/employee_train_data.csv'  # store file path
 train = pd.read_csv(train_file_path)  # read the training data
 
-y = train.ANNUAL  # set single column salePrice as target column
+y = train.Performance_Rating  # set single column salePrice as target column
 
 train_X = train[predict_cols]  # set list of columns predic_cols as predictors
 # train_X = pd.get_dummies(train_X)
-pd.get_dummies(train_X['SUBDIVISION'], prefix=['SUBDIVISION'])
-train_X = pd.concat([train_X, pd.get_dummies(train_X['SUBDIVISION'], prefix='SUBDIVISION')], axis=1)
-train_X.drop(['SUBDIVISION'], axis=1, inplace=True)
+pd.get_dummies(train_X['Designation'], prefix=['Designation'])
+train_X = pd.concat([train_X, pd.get_dummies(train_X['Designation'], prefix='Designation')], axis=1)
+train_X.drop(['Designation'], axis=1, inplace=True)
 
 print(train_X.columns)
 
 forest_model = RandomForestRegressor()  # create random forest model
 forest_model.fit(train_X, y)  # train the model using predictors and target values
 
-joblib.dump(forest_model, 'data/persistenceweather.pkl')
+joblib.dump(forest_model, 'data/persistence_employee.pkl')
